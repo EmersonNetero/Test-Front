459f0e133ffb9b79cc8db474ec91cc05ff2d5827
Emersumm <emersonbk15@gmail.com>
2022-11-20 22:54:45 -0300

    feat: produtos similares

---
+++ e-commerce/src/components/ProdutosSimilares.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="produtosSimilares">
+    <div class="container">
+      <img 
+        :src="produto.thumbnail" 
+        :key="i" 
+        alt="Produtos imagens" 
+        v-for="(produto, i) in produtos"
+        @click="produtoView(produto.id)"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'produtosSimilares',
+    props: {
+      produtos: Array
+    },
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      produtoView(id){
+        this.$router.push('/produto/' + id)
+        location.reload();
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .produtosSimilares {
+    overflow: hidden;
+    height: 130px;
+    width: 100%;
+    position: relative;
+  }
+
+  .container {
+    display: flex;
+    transition: transform 0.5s ease-in-out;
+    transform: translateX(0);
+  }
+
+  img {
+    object-fit: cover;
+    height: 200px;
+    width: 200px;
+  }
+</style>
--- e-commerce/src/views/ProdutoView.vue
+++ e-commerce/src/views/ProdutoView.vue
@@ -5,14 +5,24 @@
       <v-container>
         <v-row>
           <v-col cols="12" sm="8">
-            <v-sheet min-height="70vh" rounded="lg">
+            <v-sheet min-height="70vh" rounded="lg" v-if="produto">
               <InfoProduto :produto="produto" />
             </v-sheet>
           </v-col>
 
           <v-col cols="12" sm="4">
-            <v-sheet rounded="lg" min-height="70vh">
+            <v-sheet rounded="lg" min-height="70vh" v-if="produto">
               <CardProdutoCompra :produto="produto" />
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-container>
+        <h3>Produtos similares</h3>
+        <v-row>
+          <v-col>
+            <v-sheet rounded="lg">
+              <ProdutosSimilares :produtos="produtos"/>
             </v-sheet>
           </v-col>
         </v-row>
@@ -23,19 +33,23 @@
 
 <script>
 import Header from '@/components/Header.vue';
+import ProdutosSimilares from '@/components/ProdutosSimilares.vue';
 import InfoProduto from '@/components/InfoProduto.vue';
 import CardProdutoCompra from '@/components/CardProdutoCompra.vue';
-import { findProduto } from '@/services/api';
+import { findProduto, getProdutosCategoria } from '@/services/api';
 export default {
   name: 'ProdutoView',
   components: {
     Header,
     InfoProduto,
-    CardProdutoCompra
+    CardProdutoCompra,
+    ProdutosSimilares
   },
   data() {
     return {
-      produto: {}
+      produto: {},
+      categoria: '',
+      produtos: []
     }
   },
   methods: {
@@ -43,10 +57,16 @@ export default {
       const idProduto = this.$route.params.id;
       this.produto = await findProduto(idProduto);
       this.produto = this.produto.data;
+      this.categoria = this.produto.category;
+    },
+    async produtoSimilares() {
+      this.produtos = await getProdutosCategoria(this.categoria, 10);
+      this.produtos = this.produtos.data.products;
     }
   },
-  mounted() {
-    this.infoProduto();
+  async mounted() {
+    await this.infoProduto();
+    await this.produtoSimilares();
   }
 }
 </script>
